3f15c6b4b9afbb218722ac4d4379fd4cff395c79
Nathaniel Davenport <ncd29@cornell.edu>
2016-06-13 21:28:39 -0400

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- home page -->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>Upload An Image</title>
+</head>
+<body>
+	<!-- nav bar goes here -->
+	<?php include "includes/header.php" ?>
+	<div id="table-interface">
+		<!-- table and seats -->
+		<div id="upper">
+		</div>
+		<!-- action buttons -->
+		<div id="lower">
+		</div>
+	</div>
+
+	<div id="footer">
+	</div>
+</body>
+</html>
